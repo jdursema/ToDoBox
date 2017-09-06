829e30e400f0858cc7328ed25e003e728382edeb
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-09-06 12:18:18 -0600

    Starting Logic for Voting System

--- script.js
+++ script.js
@@ -13,12 +13,11 @@ function newCard(title, body) {
 			<p class="idea-body">
 				${body}
 			</p>
-			<p class="quality"><input type="image" alt="idea up vote button" src="images/upvote.svg" class="card-button">
+			<p class="quality"><input type="image" id="up-vote-button" alt="idea up vote button" src="images/upvote.svg" class="card-button">
 			<input type="image" id="down-vote-button" alt="idea down vote button" src="images/downvote.svg" class="card-button">quality: <span class="idea-rank">swill</span></p>
 		</article>
 		`
 	);
-	lowerQuality();
 }
 //Delete Button Function
 $('.bottom-section').on('click', '#delete-button', function(){
@@ -32,8 +31,23 @@ $('.bottom-section').on('click', '#delete-button', function(){
 // localStorage.getItem(myObject.id)
 // v
 
-function lowerQuality() {
-	var downVoteButton = $('#down-vote-button').on('click', function() {
-		$('.idea-rank').text('booty');
-	})
-} 
+// what button pressed? Are we gpomg up or down in rank? What is current rank? What is it's index? Add or subtract.
+//If at top or bottom, doesn't do anything.
+
+function changeRank(direction, currentRank) {
+	var rankArray = ['swill', 'plausible', 'genius'];
+	var increment = direction === 'down'? -1:1;
+	rankArray.indexOf(currentRank);
+	console.log(direction);
+	console.log(currentRank);
+	console.log(increment);
+}
+
+$('.bottom-section').on('click', '#up-vote-button', function() {
+	changeRank ('up',$(this).siblings('span').text()); 
+})
+
+
+$('.bottom-section').on('click', '#down-vote-button', function() {
+	changeRank ('down',$(this).siblings('span').text()); 
+})
